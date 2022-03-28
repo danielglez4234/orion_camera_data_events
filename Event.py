454c0bfc176119432bdd237290bffcd8229b9d4a
danielglez4234 <gnzlzdaniel.task@gmail.com>
2022-03-28 16:23:08 +0100

    orion json models to events types

--- Event.py
+++ Event.py
@@ -14,65 +14,166 @@ class Event:
     
 class VideoMotion(Event): 
     def toOrionEntitie(self, id): 
-        print("videoMo")
+        self.id = id
         dataJn = json.loads(self.data)
         payload = {
-            "state": {
+            "id": self.id,
+            "Type": "camera_events",
+            "Event_name": "VideoMotion",
+            "State": {
                 "type": "String",
                 "value": self.action
             },
-            "regionName": {
+            "RegionName": {
                 "type": "ArrayList",
                 "value": dataJn["RegionName"]
             }
         }
-        print(str(payload))
+        # print(str(payload))
         return payload
         
         
 class CrossRegionDetection(Event):
     def toOrionEntitie(self, id): 
+        self.id = id
         dataJn = json.loads(self.data)
         payload = {
-            "state": {
+            "id": self.id,
+            "Type": "camera_events",
+            "Event_name": "CrossRegionDetection",
+            "State": {
                 "type": "String",
                 "value": self.action
             },
-            "regionName": {
+            "DetectRegion": {
                 "type": "ArrayList",
+                "value": dataJn["DetectRegion"]
+            },
+            "ObjectDetected": {
+                "type": "Object",
+                "Action": {
+                    "type": "String",
+                    "value": dataJn["Object.Action"]
+                },
+                "ObjectType" :{
+                    "type": "String",
+                    "value": dataJn["Object.ObjectType"]
+                },
+                "Speed": {
+                    "type": "String",
+                    "value": dataJn["Object.Speed"]
+                },
+                "SpeedTypeInternal":{
+                    "type": "String",
+                    "value": dataJn["Object.SpeedTypeInternal"]
+                },
+                "FrameLocation": {
+                    "type": "Object",
+                    "BoundingBox" : {
+                        "type": "ArrayList",
+                        "value": dataJn["Object.BoundingBox"]
+                    },
+                    "Center" : {
+                        "type": "ArrayList",
+                        "value": dataJn["Object.Center"]
+                    }
+                }
             }
         }
-        print(str(payload))
+        # print(str(payload))
         return payload
 
 
 
 class CrossLineDetection(Event):
     def toOrionEntitie(self, id): 
+        self.id = id
+        dataJn = json.loads(self.data)
         payload = {
-            "state": {
+            "id": self.id,
+            "Type": "camera_events",
+            "Event_name": "CrossLineDetection",
+            "State": {
                 "type": "String",
                 "value": self.action
             },
-            "regionName": {
+            "DetectLine": {
                 "type": "ArrayList",
+                "value": dataJn["DetectLine"]
+            },
+            "Direction": {
+                "type": "String",
+                "value": dataJn["Direction"]
+            },
+            "ObjectDetected": {
+                "type": "Object",
+                "Action": {
+                    "type": "String",
+                    "value": dataJn["Object.Action"]
+                },
+                "ObjectType" :{
+                    "type": "String",
+                    "value": dataJn["Object.ObjectType"]
+                },
+                "Speed": {
+                    "type": "String",
+                    "value": dataJn["Object.Speed"]
+                },
+                "SpeedTypeInternal":{
+                    "type": "String",
+                    "value": dataJn["Object.SpeedTypeInternal"]
+                },
+                "FrameLocation": {
+                    "type": "Object",
+                    "BoundingBox" : {
+                        "type": "ArrayList",
+                        "value": dataJn["Object.BoundingBox"]
+                    },
+                    "Center" : {
+                        "type": "ArrayList",
+                        "value": dataJn["Object.Center"]
+                    }
+                }
             }
         }
-        print(str(payload))
+        # print(str(payload))
         return payload
 
 
 class TakenAwayDetection(Event):
     def toOrionEntitie(self, id): 
+        self.id = id
+        dataJn = json.loads(self.data)
         payload = {
-            "state": {
+            "id": self.id,
+            "Type": "camera_events",
+            "Event_name": "TakenAwayDetection",
+            "State": {
                 "type": "String",
                 "value": self.action
             },
-            "regionName": {
-                "type": "ArrayList",
-  
+            "ObjectDetected": {
+                "type": "Object",
+                "Action": {
+                    "type": "String",
+                    "value": dataJn["Object.Action"]
+                },
+                "ObjectType" :{
+                    "type": "String",
+                    "value": dataJn["Object.ObjectType"]
+                },
+                "FrameLocation": {
+                    "type": "Object",
+                    "BoundingBox" : {
+                        "type": "ArrayList",
+                        "value": dataJn["Object.BoundingBox"]
+                    },
+                    "Center" : {
+                        "type": "ArrayList",
+                        "value": dataJn["Object.Center"]
+                    }
+                }
             }
         }
-        print(str(payload))
+        # print(str(payload))
         return payload
